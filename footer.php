04bf41d4863cb17fc724d9b2ccf74ffea9a2aae3
Ewelina <ewelina.muc@gmail.com>
2014-10-19 12:18:11 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+    <!-- Footer -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+            <div class="blog-footer">
+              <p>Szablon WordPress zbudowany na <a href="http://getbootstrap.com">Bootstrap 3</a> przez <a href="https://twitter.com/ewelina_muc">@ewelina_muc</a> na <a href="http://worduplodz.pl/">WordUp Łódź</a></p>
+              <p>Copywrite &copy; <?php echo date("Y") ?></p>
+            </div>
+        </div>
+      </div><!-- /.row -->
+    </div><!-- /.container -->
+<?php wp_footer() ?>
+  </body>
+</html>
